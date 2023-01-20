305f9d7fad6d109ae2a551cb87ccfeddb89475ab
LOLO <lotaijo@gmail.com>
2023-01-20 22:21:38 +1300

    api routing2

--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -1,16 +1,23 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const SingleEvent = ({ data }) => {
   const inputEmail = useRef();
-
   const router = useRouter();
+  const [message, setMessage] = useState("");
 
   const onSubmit = async (e) => {
     e.preventDefault();
     const emailValue = inputEmail.current.value;
     const eventId = router?.query.id;
+
+    const validRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (!emailValue.match(validRegex)) {
+      setMessage("Please introduce a correct email address");
+    }
 
     try {
       const response = await fetch("/api/email-registration", {
@@ -23,7 +30,7 @@ const SingleEvent = ({ data }) => {
 
       if (!response.ok) throw new Error(`Error: ${response.status}`);
       const data = await response.json();
-
+      setMessage(data.message);
       inputEmail.current.value = "";
     } catch (e) {
       console.log("ERROR", e);
@@ -32,19 +39,20 @@ const SingleEvent = ({ data }) => {
 
   return (
     <div className="event_single_page">
-      <Image src={data.image} alt={data.title} width={800} height={500}></Image>
-      <h1> {data.title}</h1>
-      <p>{data.description}</p>
+      <h1> {data.title} </h1>
+      <Image src={data.image} width={1000} height={500} alt={data.title} />
+      <p> {data.description} </p>
       <form onSubmit={onSubmit} className="email_registration">
-        <label>Get Registered for this Event! </label>
+        <label> Get Registered for this event!</label>
         <input
           ref={inputEmail}
           type="email"
           id="email"
-          placeholder="Please insert your eamil here"
+          placeholder="Please insert your email here"
         />
-        <button type="">Submit</button>
+        <button type="submit"> Submit</button>
       </form>
+      <p>{message}</p>
     </div>
   );
 };
